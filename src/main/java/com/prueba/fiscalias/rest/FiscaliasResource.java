68aebc2189685f79dd9a73424ab7536635c58688
Dary Castillo <darycastillo27@gmail.com>
2022-03-03 09:54:38 -0600

    CROS origins

--- src/main/java/com/prueba/fiscalias/rest/FiscaliasResource.java
+++ src/main/java/com/prueba/fiscalias/rest/FiscaliasResource.java
@@ -20,7 +20,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import static javax.ws.rs.core.Response.status;
 
 /**
  * REST Web Service
@@ -67,6 +66,7 @@ public class FiscaliasResource {
     }
 
     @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
     public Response delete(@PathParam("id") int fiscaliaId) throws SQLException, ClassNotFoundException {
